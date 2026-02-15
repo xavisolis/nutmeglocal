95beebdd7cf18aae39acb9e5d43bf9a1954d1417
jaimesolis <jaimesolisa@gmail.com>
2026-02-14 19:34:48 -0500

    providers + deps update

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,11 +1,22 @@
 'use client';
 
 import { useEffect } from 'react';
+import Lenis from 'lenis';
 import { initPostHog } from '@/lib/posthog';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     initPostHog();
+  }, []);
+
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+    return () => lenis.destroy();
   }, []);
 
   return <>{children}</>;
